e2527557fcc40787ead7970b264e034c1788f815
etiennepro <etiennepro@linux-c7qx.eduroam.emn.fr>
2020-03-08 19:36:39 +0100

    Add constraint 4.2

--- CADOR/src/modeleDeux.java
+++ CADOR/src/modeleDeux.java
@@ -24,7 +24,7 @@ public class modeleDeux {
 	// 3 : Nuit
 	// 4 : JCA
 	// 5 : Vide
-	public static final List<Integer> creneaux = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6));
+	public static final List<Integer> creneaux = new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5));
 	
 	public static IntVar kronecker(ArrayList<Integer> integer,IntVar Plannif) {
 		IntVar kroneck=model.intVar("kroneck",0,1,true);
@@ -36,12 +36,6 @@ public class modeleDeux {
 		
 	public static void main(String[] args) {
 		int[] horizons= {1,2,3};
-		
-		
-		
-		
-
-		
 		
 		//Paramètres
 		
@@ -82,25 +76,19 @@ public class modeleDeux {
 		int reposHebdo=6;
 		
 		
-		
 		//Variables
 		IntVar[][] Plannifs=model.intVarMatrix("Plannification", nbAgents, H, 0,6);
-		
 		
 		
 		//Contraintes
 		
 		//Contrainte 3.2
+		
 		for (int p=0;p<(int)(H/7);p++) {
 			for (int k=0; k<=nbAgents; k++) {
 				IntVar[] tabKron= new IntVar[7];
 				for (int i=0;i<7;i++) {
-					// Copie les créneaux
-					ArrayList<Integer> Domaine = new ArrayList<Integer>(creneaux);
-					// Restreint le domaine aux valeurs voulues pour la contrainte
-					// ici : toutes les valeurs sauf vide 
-					Domaine.remove(5);
-					tabKron[i]=kronecker(Domaine, Plannifs[k][7*p+i]);
+					tabKron[i]=kronecker(new ArrayList<Integer>(Arrays.asList(0,1,2,3,4)), Plannifs[k][7*p+i]);
 				}
 				
 				model.sum(tabKron,"<=",5).post();
@@ -113,24 +101,35 @@ public class modeleDeux {
 				for (int k=0; k<=nbAgents; k++) {
 					IntVar[] tabKron= new IntVar[7];
 					for (int i=0;i<7;i++) {
-						ArrayList<Integer> Domaine = new ArrayList<Integer>(creneaux);
-						Domaine.remove(5);
-						tabKron[i]=kronecker(Domaine, Plannifs[k][p+i]);
+						tabKron[i]=kronecker(new ArrayList<Integer>(Arrays.asList(0,1,2,3,4)), Plannifs[k][p+i]);
 					}
 						
 					model.sum(tabKron,"<=",8).post();//
 				}
 			}
 				
+		// Contrainte 4.2
+			
+		for(int k=0;k<nbAgents;k++) {
+			for(int j=0;j<H-1;j++) {
+				IntVar[][] tabKron = new IntVar[2][2];
+				tabKron[0][0] = kronecker(new ArrayList<Integer>(Arrays.asList(3)), Plannifs[k][j]);
+				tabKron[1][0] = kronecker(new ArrayList<Integer>(Arrays.asList(0,1)), Plannifs[k][j+1]);
+				tabKron[0][1] = kronecker(new ArrayList<Integer>(Arrays.asList(2)), Plannifs[k][j]);
+				tabKron[1][1] = kronecker(new ArrayList<Integer>(Arrays.asList(0)), Plannifs[k][j+1]);
 				
+				// model produit tabKron[0][0]*tabKron[0][1] == 0
+				// model produit tabKron[1][0]*tabKron[1][1] == 0
+			}
+		}
+			
+			
 		//Contrainte 4.4
 		for (int p=0;p<((int)H/7)-1;p++) {
 			for (int k=0; k<=nbAgents; k++) {
 				IntVar[] tabKron= new IntVar[14];
 				for (int i=0;i<14;i++) {
-					ArrayList<Integer> Domaine = new ArrayList<Integer>();
-					Domaine.add(6);
-					tabKron[i]=kronecker(Domaine, Plannifs[k][7*p+i]);
+					tabKron[i]=kronecker(new ArrayList<Integer>(Arrays.asList(5)), Plannifs[k][7*p+i]);
 				}
 				
 				model.sum(tabKron,">=",4).post();
@@ -149,7 +148,6 @@ public class modeleDeux {
 				model.sum(tabKron,">=",4).post();
 			}
 		}
-		
 		
 		
 		
